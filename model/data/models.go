9a29ee5eceb7f1c4e803c7f73729d43112369bc2
palatipw <palatip.wongyeekul@scb.co.th>
2022-04-20 10:54:17 +0700

    test heroku

--- model/data/models.go
+++
@@ -1,7 +0,0 @@
-package data
-
-type NewData struct {
-	Unique_id string `json:"unique_id" binding:"required"`
-	Name string `json:"name" binding:"required"`
-	Age uint `json:"age" binding:"required"`
-}
